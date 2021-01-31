5b0436e3f639ca3188e52e0e661449e0cd9696f7
hannanel gershinsky <hannanel100@gmail.com>
2021-01-31 19:09:06 +0200

    upto 1:43:58

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const body = document.querySelector("body");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  console.log("open hamburger");
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+    body.classList.remove("noscroll");
+  } else {
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+    body.classList.add("noscroll");
+  }
+});
